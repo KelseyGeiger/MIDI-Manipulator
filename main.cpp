20967a218a65b7f05562d2112ff033bc75df913b
Kiel Geiger <kiel.geiger895@myci.csuci.edu>
2016-01-19 23:53:40 -0800

    Implemented fully functional binary MIDI reading and writing.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_main.h"
 #include "MIDI_VLQ.hpp"
-#include "WaveSynth.hpp"
-#include "StringSynth.hpp"
+#include "MIDI_Chunk.hpp"
 #include "GuitarSynth.hpp"
 #include "Rendering.h"
 #include <thread>
@@ -23,27 +22,35 @@ int main(int argc, char* argv[])
 	SDL_Window* win = SDL_CreateWindow("MIDI Player", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
 
 	if(!win) {
-		return 1;
+		return -1;
 	}
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+	std::fstream some_midi;
+    some_midi.open("./assets/BringItIn,Guys!.mid", std::ios::in | std::ios::binary);
+    std::fstream midi_copy;
+    midi_copy.open("./test/midi_copy.mid", std::ios::out | std::ios::trunc | std::ios::binary);
 
-	if(!renderer) {
-		return 2;
+    if(!some_midi) {
+		return -2;
+    }
+
+	MIDI_Chunk header_chunk;
+	some_midi >> header_chunk;
+    midi_copy << header_chunk;
+    midi_copy.flush();
+
+	std::vector<MIDI_Chunk> tracks;
+
+	for(uint32_t i = 0; i < header_chunk.GetHeader().track_count; i++) {
+		MIDI_Chunk tmp{};
+		some_midi >> tmp;
+		midi_copy << tmp;
+		midi_copy.flush();
+		tracks.push_back(tmp);
 	}
-
-	GuitarSynth synth;
-	synth.SetVolume(1.0f);
-
-	synth.Play();
 
 	SDL_Event evt;
 	bool quit = false;
-	bool paused = false;
-	bool stopped = false;
-	bool should_draw = true;
-
-	bool string_plucked[6] = { false };
 
 	int32_t d_time = 0;
 	std::chrono::steady_clock::time_point prev = std::chrono::steady_clock::now();
@@ -56,93 +63,16 @@ int main(int argc, char* argv[])
 
         d_time += std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
 
-		if(should_draw) {
-			SoundSample samp = synth.GenerateSample(48000, 25, d_time);
-			DrawSampleWaveform(renderer, samp);
-		} else {
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			SDL_RenderClear(renderer);
-			SDL_RenderPresent(renderer);
-		}
-
-		Chord chord;
-
 		while(SDL_PollEvent(&evt)) {
 			if(evt.type == SDL_KEYDOWN) {
 				if(evt.key.keysym.sym == SDLK_ESCAPE) {
 					quit = true;
-				} else if(evt.key.keysym.sym == SDLK_d) {
-					should_draw = !should_draw;
-				} else if(evt.key.keysym.sym == SDLK_p) {
-					paused = !paused;
-
-					if(paused) {
-						synth.Pause();
-					} else {
-						synth.Unpause();
-					}
-				} else if(evt.key.keysym.sym == SDLK_s) {
-					stopped = !stopped;
-
-					if(stopped) {
-						synth.Stop();
-					} else {
-						synth.Play();
-					}
-				} else if(evt.key.keysym.sym == SDLK_SPACE) {
-					synth.Strum();
-				} else if(evt.key.keysym.sym == SDLK_o) {
-					for(uint32_t i = 1; i <= 6; i++) {
-						synth.OpenString(i);
-					}
-				} else if(evt.key.keysym.sym == SDLK_q && !string_plucked[0]) {
-					string_plucked[0] = true;
-					chord.AddNote(Note(Note::C, Note::SHARP, 5));
-				} else if(evt.key.keysym.sym == SDLK_w && !string_plucked[1]) {
-					string_plucked[1] = true;
-					chord.AddNote(Note(Note::A, Note::FLAT, 4));
-				} else if(evt.key.keysym.sym == SDLK_e && !string_plucked[2]) {
-					string_plucked[2] = true;
-					chord.AddNote(Note(Note::E, Note::NATURAL, 4));
-				} else if(evt.key.keysym.sym == SDLK_r && !string_plucked[3]) {
-					string_plucked[3] = true;
-					chord.AddNote(Note(Note::B, Note::NATURAL, 3));
-				} else if(evt.key.keysym.sym == SDLK_t && !string_plucked[4]) {
-					string_plucked[4] = true;
-					chord.AddNote(Note(Note::F, Note::SHARP, 3));
-				} else if(evt.key.keysym.sym == SDLK_y && !string_plucked[5]) {
-					string_plucked[5] = true;
-					chord.AddNote(Note(Note::C, Note::SHARP, 3));
-				} else if(evt.key.keysym.sym == SDLK_EQUALS) {
-					float vol = synth.GetVolume();
-					synth.SetVolume(vol + 0.01f);
-				} else if(evt.key.keysym.sym == SDLK_MINUS) {
-					float vol = synth.GetVolume();
-					synth.SetVolume(vol - 0.01f);
-				}
-			} else if(evt.type == SDL_KEYUP) {
-				if(evt.key.keysym.sym == SDLK_q && string_plucked[0]) {
-					string_plucked[0] = false;
-				} else if(evt.key.keysym.sym == SDLK_w && string_plucked[1]) {
-					string_plucked[1] = false;
-				} else if(evt.key.keysym.sym == SDLK_e && string_plucked[2]) {
-					string_plucked[2] = false;
-				} else if(evt.key.keysym.sym == SDLK_r && string_plucked[3]) {
-					string_plucked[3] = false;
-				} else if(evt.key.keysym.sym == SDLK_t && string_plucked[4]) {
-					string_plucked[4] = false;
-				} else if(evt.key.keysym.sym == SDLK_y && string_plucked[5]) {
-					string_plucked[5] = false;
 				}
 			} else if(evt.type == SDL_QUIT) {
 				quit = true;
 			}
 		}
-
-		synth.PlayChord(chord);
 	}
-
-	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
 
